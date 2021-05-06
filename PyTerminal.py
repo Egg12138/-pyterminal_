8ad7311877ccd8d7030f8e5c3bfbe79ff169bc4a
Egg12138 <38132747+Egg12138@users.noreply.github.com>
2021-05-06 23:14:08 +0800

    Add files via upload

---
+++ PyTerminal.py
@@ -0,0 +1,15 @@
+#python terminal
+
+import argparse
+import sys
+import os
+import random
+
+parser = argparse.ArgumentParser(description='Process ')
+
+
+
+if __name__ == "__main__":#When directly running...
+    usr = sys.argv[2]
+    cur_dir = "/mnt/c/user"#current directory
+    command = intput()
